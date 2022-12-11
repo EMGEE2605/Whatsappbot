f75ec2d90747ff88ab11fc59f4f13e1d0ecc5fad
Moses George <mosesgeorge@Mosess-MacBook-Pro.local>
2022-12-11 08:12:18 +0100

    new line of code

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -3,11 +3,35 @@ from whatsapp import Client
 # Create a new instance of the WhatsApp client
 client = Client()
 
+#Define phone number
+PHONE_NUMBER = "+2348173854807"
+
+#Define AUTH_CODE
+AUTH_CODE = "260805"
+
 # Define the bot's name
 BOT_NAME = "Jessy"
 
+#Define group name
+GROUP_NAME = ""
+
+#Define group recipient
+RECIPIENT = ""
+
 # Connect to WhatsApp using your phone number and authentication code
 client.connect(PHONE_NUMBER, AUTH_CODE)
+
+# Get a list of groups that the bot is a member of
+groups = client.get_groups()
+
+# Find the group chat with the specified name
+group = next((group for group in groups if group.name == GROUP_NAME), None)
+
+# Define the group chat ID
+GROUP_ID = group.id
+
+#Define search for answer
+search_for_answer = ""
 
 # Add the bot to a group chat
 client.add_to_group(GROUP_ID, BOT_NAME)
@@ -22,7 +46,8 @@ def on_message_received(message):
   if message.text.startswith(BOT_NAME):
   
   # Check if the message is a question
-  if message.text.endswith("?"):
+   if message.text.endswith("?"):
+
     # Use a search engine or other online service to find the answer to the question
     answer = search_for_answer(message.text)
 
